14bb6016110ad336b9f722df9afc1c5efb0947c7
Vanya01 <good.boy.2k19@gmail.com>
2021-10-24 14:58:15 +0300

    Done

--- src/Components/search/Search.js
+++ src/Components/search/Search.js
@@ -7,99 +7,89 @@ import {Pagination} from "@material-ui/lab";
 import {MoviesInfo} from "../MoviesListCard/info";
 
 
-
 const Search = () => {
-    const [type,setType] = useState(0)
-    const [page,setPage] = useState(1)
-    const [searchText,setSearchText]= useState("")
-    const [content,setConten] = useState()
-    const [numOfPages,setNumOfPages] = useState()
+    const [type, setType] = useState(0)
+    const [page, setPage] = useState(1)
+    const [searchText, setSearchText] = useState("")
+    const [content, setConten] = useState()
+    const [numOfPages, setNumOfPages] = useState()
 
 
-    const fetchSearch = async ()=>{
-  const { data } = await axios.get(
-    `   https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${process.API_KEY}&language=en-US&query=${searchText}&page=${page}&include_adult=false`
-   )
+    const fetchSearch = async () => {
+        const {data} = await axios.get(
+            `   https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${process.API_KEY}&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+        )
 
         setConten(data.results)
         setNumOfPages(data.totalPages)
     }
 
-    useEffect(()=>{
-        window.scroll(0,0);
+    useEffect(() => {
+        window.scroll(0, 0);
         fetchSearch();
         // eslint-disable-next-line
-    },[type,page])
+    }, [type, page])
 
 
-    return(
+    return (
         <div>
             <div>
-       <TextField
-        style={{flex:1,width:550}}
-        className={'searchBox'}
-        label={'Search'}
-        variant={'filled'}
-        onChange={(e)=> setSearchText(e.target.value)}
-       />
+                <TextField
+                    style={{flex: 1, width: 550}}
+                    className={'searchBox'}
+                    label={'Search'}
+                    variant={'filled'}
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
 
-    <button variant={'contained'} style={{marginLeft:10}}  onClick={fetchSearch} ><SearchIcon/></button>
+                <button variant={'contained'} style={{marginLeft: 10}} onClick={fetchSearch}><SearchIcon/></button>
 
             </div>
 
             <Tabs value={type}
                   indicatorColor="primary"
                   textColor="primary"
-                  onChange={(event,newValue)=>{
+                  onChange={(event, newValue) => {
                       setType(newValue);
                       setPage(1)
                   }}
-                  style={{paddingBottom:5}}
+                  style={{paddingBottom: 5}}
 
             >
 
-                 <Tab style={{width:'50%'}} label={'Search movies'}/>
-                 <Tab style={{width:'50%'}} label={'Search TV'}/>
+                <Tab style={{width: '50%'}} label={'Search movies'}/>
+                <Tab style={{width: '50%'}} label={'Search TV'}/>
 
             </Tabs>
 
 
-              <div className={'searchContent'}>
+            <div className={'searchContent'}>
 
-                  {
-                      content && content.map((c)=>(
-                          <MoviesInfo
-                             key={c.id}
-                             id={c.id}
-                             poster={c.poster_path}
-                             title={c.title || c.name}
-                             date={c.first_air_date || c.release_date}
-                             media_type={type ? "tv" : "movie"}
-       vote_average={c.vote_average} />
-                      ))}
-              </div>
+                {
+                    content && content.map((c) => (
+                        <MoviesInfo
+                            key={c.id}
+                            id={c.id}
+                            poster={c.poster_path}
+                            title={c.title || c.name}
+                            date={c.first_air_date || c.release_date}
+                            media_type={type ? "tv" : "movie"}
+                            vote_average={c.vote_average}/>
+                    ))}
+            </div>
             {numOfPages > 1 && (
                 <Pagination setPage={setPage} numOfPages={numOfPages}/>
             )}
 
-                  {
-                      searchText && !content && (type ? <h2> No series found</h2> : <h2> No movies found</h2>)
-                  }
-            </div>
+            {
+                searchText && !content && (type ? <h2> No series found</h2> : <h2> No movies found</h2>)
+            }
+        </div>
     )
 
 
 }
 export default Search
-
-
-
-
-
-
-
-
-
 
 
 //
